4e7ad618d96a16c23fc1512874e7785354736c37
KhanhToan <KhanhToan@DESKTOP-C6JGQK7>
2020-12-27 15:40:30 +0700

    commit

--- PatientManagement/Areas/Admin/Models/StatisticPatient.cs
+++ PatientManagement/Areas/Admin/Models/StatisticPatient.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace PatientManagement.Areas.Doctor.Models
+namespace PatientManagement.Areas.Admin.Models
 {
     public class StatisticPatient
     {
